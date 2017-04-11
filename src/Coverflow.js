ffc51462b5698d26c4585aea1e413d82f43adedc
Ranjan Shrestha <ranjan@sharingapples.com>
2017-04-11 22:23:52 +0545

    Configure library project

---
+++ src/Coverflow.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class Coverflow extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+export default Coverflow;
