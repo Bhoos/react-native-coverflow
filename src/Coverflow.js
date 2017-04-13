f21b08bf0d48416aa0de61a39d2bf54e1b5e9ef0
Ranjan Shrestha <ranjan@sharingapples.com>
2017-04-13 15:09:08 +0545

    First draft

--- src/Coverflow.js
+++ src/Coverflow.js
@@ -1,10 +1,271 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
+import React, { Component, PropTypes, Children } from 'react';
+import { Animated, View, PanResponder, StyleSheet, Platform } from 'react-native';
+
+import Item from './Item';
+
+const SENSITIVITY_LOW = 'low';
+const SENSITIVITY_HIGH = 'high';
+const SENSITIVITY_NORMAL = 'normal';
+
+function convertSensitivity(sensitivity) {
+  switch (sensitivity) {
+    case SENSITIVITY_LOW:
+      return 120;
+    case SENSITIVITY_HIGH:
+      return 40;
+    case SENSITIVITY_NORMAL:
+    default:
+      return 60;
+  }
+}
+
+function mapPropsToState(props, { width, selection }) {
+  const sel = selection || 0;
+  return {
+    pageWidth: convertSensitivity(props.sensitivity),
+    children: fixOrder(Children.toArray(props.children), sel),
+    width,
+  };
+}
+
+function getAnimatedValue(v) {
+  // eslint-disable-next-line no-underscore-dangle
+  return v.__getValue();
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+function clamp(value, min, max) {
+  if (value < min) {
+    return min;
+  }
+
+  if (value > max) {
+    return max;
+  }
+
+  return value;
+}
+
+function fixOrder(items, selection) {
+  // Fix the order of children
+  const children = [];
+  for (let i = 0; i < selection; i += 1) {
+    children.push([i, items[i]]);
+  }
+
+  for (let i = items.length - 1; i > selection; i -= 1) {
+    children.push([i, items[i]]);
+  }
+
+  children.push([selection, items[selection]]);
+
+  return children;
+}
 
 class Coverflow extends Component {
-  render() {
+  static propTypes = {
+    style: View.propTypes.style,
+    sensitivity: PropTypes.oneOf([SENSITIVITY_LOW, SENSITIVITY_NORMAL, SENSITIVITY_HIGH]),
+    initialSelection: PropTypes.number,
+    spacing: PropTypes.number,
+    wingSpan: PropTypes.number,
+    rotation: PropTypes.number,
+    midRotation: PropTypes.number,
+    perspective: PropTypes.number,
+    scaleDown: PropTypes.number,
+    scaleFurther: PropTypes.number,
+    children: PropTypes.arrayOf(PropTypes.element).isRequired,
+    onChange: PropTypes.func.isRequired,
+  };
+
+  static defaultProps = {
+    initialSelection: 0,
+    style: undefined,
+    sensitivity: SENSITIVITY_NORMAL,
+    spacing: 100,
+    wingSpan: 300,
+    rotation: 50,
+    midRotation: 50,
+    perspective: 800,
+    scaleDown: 0.8,
+    scaleFurther: 0.75,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      width: 0,
+      scrollX: new Animated.Value(0),
+      selection: props.initialSelection,
+      children: fixOrder(Children.toArray(props.children), props.initialSelection),
+    };
+  }
+
+  componentWillMount() {
+    let offset = 0;
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => (
+        // Since we want to handle presses on individual items as well
+        // Only start the pan responder when there is some movement
+        Math.abs(gestureState.dx) > 15
+      ),
+      onPanResponderGrant: () => {
+        offset = getAnimatedValue(this.state.scrollX);
+      },
+      onPanResponderTerminationRequest: () => true,
+      onPanResponderMove: (evt, gestureState) => {
+        // change the scroll value with the drag event
+        const { pageWidth, scrollX } = this.state;
+        scrollX.setValue(offset - (gestureState.dx / pageWidth));
+        console.log('Velocity', gestureState.vx, gestureState.dx);
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        const { pageWidth, scrollX } = this.state;
+        const count = Children.count(this.props.children);
+        // console.log('Release Velocity', gestureState.vx, gestureState.dx);
+        // const velocity = Math.abs(gestureState.vx);
+        // const deceleration = 0.5;
+
+
+        // const { pageWidth, scrollX } = this.state;
+        // // TODO: Calculate displacement based on velocity
+        // const time = velocity / deceleration;
+        // console.log('Calculated Time', time);
+        // const displacement = Math.sign(gestureState.vx) * ((velocity * time) - ((deceleration * time * time) / 2));
+        // console.log('Additional Displacement', displacement, 'from', scrollX.__getValue());
+        // const finalPos = Math.min(count - 1,
+        //   Math.max(0,
+        //     Math.round(scrollX.__getValue() - (displacement))));
+        // console.log('Final Position', finalPos);
+        // const s = scrollX.__getValue() - finalPos;
+        // const a = (velocity * velocity) / (2 * s);
+        // console.log('Effective Deceleration', a);
+        // const t = Math.abs(velocity / a);
+        // console.log('Using time', t);
+
+        const pageVelocity = gestureState.vx / pageWidth;
+        if (scrollX.__getValue() > 0 && scrollX.__getValue() < count && Math.abs(pageVelocity) > (1 / pageWidth)) {
+          const velocity = Math.sign(gestureState.vx) * clamp(Math.abs(pageVelocity), 1 / pageWidth, 5 / pageWidth);
+          console.log('Velocity', velocity);
+          Animated.decay(scrollX, {
+            velocity: -velocity,
+            deceleration: 0.99,
+          }).start(() => {
+            console.log('Decay complete');
+            const pos = clamp(Math.round(scrollX.__getValue()), 0, count - 1);
+            Animated.spring(scrollX, {
+              toValue: pos,
+            }).start();
+          });
+        } else {
+          const pos = clamp(Math.round(scrollX.__getValue()), 0, count - 1);
+          Animated.spring(scrollX, {
+            toValue: pos,
+          }).start();
+        }
+
+        // // Snap to an item
+        // Animated.spring(scrollX, {
+        //   toValue: finalPos,
+        // }).start();
+      },
+    });
+
+    this.listenerId = this.state.scrollX.addListener(this.fixChildOrder);
+  }
+
+  fixChildOrder = ({ value }) => {
+    const { children } = this.props;
+
+    const newSelection = clamp(Math.round(value), 0, Children.count(children) - 1);
+    if (newSelection !== this.state.selection) {
+      this.setState({
+        selection: newSelection,
+        children: fixOrder(Children.toArray(this.props.children), newSelection),
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    this.state.scrollX.removeListener(this.listenerId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(mapPropsToState(nextProps, this.state));
+  }
+
+  onLayout = ({ nativeEvent }) => {
+    const state = mapPropsToState(this.props, { width: nativeEvent.layout.width });
+    this.setState(state);
+  }
+
+  select = (idx) => {
+    const { scrollX } = this.state;
+    Animated.spring(scrollX, {
+      toValue: idx,
+    }).start();
+  }
+
+  renderItem = ([position, item]) => {
+    if (!this.state.width) {
+      return null;
+    }
+
+    const { scrollX } = this.state;
+    const { rotation, midRotation, perspective, children, scaleDown, scaleFurther, spacing, wingSpan } = this.props;
+    const count = Children.count(children);
+
     return (
-      <View />
+      <Item
+        key={item.key}
+        scroll={scrollX}
+        position={position}
+        count={count}
+        spacing={spacing}
+        wingSpan={wingSpan}
+        rotation={rotation}
+        midRotation={midRotation}
+        perspective={perspective}
+        scaleDown={scaleDown}
+        scaleFurther={scaleFurther}
+        onSelect={this.select}
+      >
+        {item}
+      </Item>
+    );
+  }
+
+  render() {
+    const {
+      style,
+      rotation,
+      midRotation,
+      scaleDown,
+      scaleFurther,
+      perspective,
+      spacing,
+      wingSpan,
+      ...props
+    } = this.props;
+    const { children } = this.state;
+    console.log('Rendering children', children.length);
+    return (
+      <View
+        style={[styles.container, style]}
+        {...props}
+        onLayout={this.onLayout}
+        {...this.panResponder.panHandlers}
+      >
+        {children.map(this.renderItem)}
+      </View>
     );
   }
 }
---
+++ src/Item.js
@@ -0,0 +1,104 @@
+import React, { Component, PropTypes } from 'react';
+import { Animated, TouchableWithoutFeedback, Text, View } from 'react-native';
+
+let  num = 0;
+
+class Item extends Component {
+  static propTypes = {
+    scroll: PropTypes.instanceOf(Animated.Value).isRequired,
+    count: PropTypes.number.isRequired,
+    position: PropTypes.number.isRequired,
+    children: PropTypes.element.isRequired,
+    wingSpan: PropTypes.number.isRequired,
+    spacing: PropTypes.number.isRequired,
+    rotation: PropTypes.number.isRequired,
+    midRotation: PropTypes.number.isRequired,
+    perspective: PropTypes.number.isRequired,
+    scaleDown: PropTypes.number.isRequired,
+    scaleFurther: PropTypes.number.isRequired,
+    onSelect: PropTypes.func.isRequired,
+  };
+
+  static childContextTypes = {
+    animatedPosition: PropTypes.instanceOf(Animated.Value),
+  };
+
+  getChildContext() {
+    return {
+      animatedPosition: null,
+    };
+  }
+
+  render() {
+    const {
+      scroll,
+      position,
+      rotation,
+      midRotation,
+      perspective,
+      count,
+      scaleDown,
+      scaleFurther,
+      wingSpan,
+      spacing,
+      onSelect,
+    } = this.props;
+    const extremeLeft = Math.min(position - 2, position - (count / 2));
+    const extremeRight = Math.max(position + 2, position + (count / 2));
+    // console.log(temp);
+    // temp.addListener((v) => {
+    //   console.log('Value is', v);
+    // });
+
+    const style = {
+      transform: [
+        { perspective },
+        {
+          translateX: scroll.interpolate({
+            inputRange: [extremeLeft, position - 1, position, position + 1, extremeRight],
+            outputRange: [wingSpan, spacing, 0, -spacing, -wingSpan],
+          }),
+        },
+        {
+          scale: scroll.interpolate({
+            inputRange: [position - 2, position - 1, position, position + 1, position + 2],
+            outputRange: [scaleFurther, scaleDown, 1, scaleDown, scaleFurther],
+          }),
+        },
+        {
+          rotateY: scroll.interpolate({
+            inputRange: [position - 2, position - 1, position - 0.5, position, position + 0.5, position + 1, position + 2],
+            outputRange: [`-${rotation}deg`, `-${rotation}deg`, `-${midRotation}deg`, '0deg', `${midRotation}deg`, `${rotation}deg`, `${rotation}deg`],
+          }),
+        },
+      ],
+    };
+
+    return (
+      <Animated.View
+        pointerEvents="box-none"
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          right: 0,
+          bottom: 0,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <TouchableWithoutFeedback
+          onPressIn={() => console.log('On press in')}
+          onPressOut={() => console.log('On Press out')}
+          onPress={() => onSelect(position)}
+        >
+          <Animated.View style={style}>
+            {this.props.children}
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      </Animated.View>
+    );
+  }
+}
+
+export default Item;
